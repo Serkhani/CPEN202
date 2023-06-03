d57d23c1f8924b3f78d37221b76cf5988fa22067
Serkhani <deviper03@gmail.com>
2023-06-03 22:16:47 +0000

    added nor and compiled xnor

--- Lab1/norgate.vhd
+++ Lab1/norgate.vhd
@@ -1,1 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
 
+entity nor_gate is
+    port(A: in std_logic;     
+         B: in std_logic;     
+         Y: out std_logic); 
+end nor_gate; 
+
+architecture norArch of nor_gate is
+ begin
+    Y <= not(A OR B);
+end norArch;
