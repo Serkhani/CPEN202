848b14db487ecd4efabfe4287521a3d0c66e3180
Serkhani <deviper03@gmail.com>
2023-06-03 22:18:15 +0000

    added nand and compiled xnor, nor

--- Lab1/nandgate.vhd
+++ Lab1/nandgate.vhd
@@ -1,1 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
 
+entity nand_gate is
+    port(A: in std_logic;      
+         B: in std_logic;     
+         Y: out std_logic); 
+end nand_gate;
+
+architecture nandArch of nand_gate is
+begin
+    Y <= not (A and B);
+end nandArch;
